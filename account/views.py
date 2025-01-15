aea09dcde5ebeb098f2f9b24e7bef3ff7ab498b2
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-15 18:33:05 +0300

    Wrote view for registration.

--- account/views.py
+++ account/views.py
@@ -22,3 +22,16 @@ def login_view(request):
 
 	context = {'form': form}
 	return render(request, 'account/login.html', context)
+
+
+def register_view(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+	else:
+		form = UserCreationForm()
+
+	context = {'form': form}
+	return render(request, 'account/register.html', context)
