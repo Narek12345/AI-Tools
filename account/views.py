3c62aa01c11b443a8020a8bfc5094614de54228c
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-15 18:35:14 +0300

    Fixed a bug: added import UserCreationForm.

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login_view(request):
