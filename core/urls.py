1dd2a80e4997a54fdee3135674a446ca61251a24
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-18 11:48:28 +0300

    Create view and html page for project item.

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ app_name = 'core'
 urlpatterns = [
 	path('', views.home, name='home'),
 	path('about', views.about, name='about'),
+	path('list_projects', views.list_projects, name='list_projects'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,7 @@ def home(request):
 
 def about(request):
 	return render(request, 'core/about.html')
+
+
+def list_projects(request):
+	return render(request, 'core/list_projects.html')
