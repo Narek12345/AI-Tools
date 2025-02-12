3d3afc897b9905a250d5de5f717e64bfa724d5e1
Narek <narekbayanduryan16@gmail.com>
2025-02-12 11:17:04 +0300

    Wrote a unit test for the home page.

---
+++ telegram_app/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+
+class HomePageTest(TestCase):
+	"""Тест домашней страницы."""
+
+
+	def test_uses_home_template(self):
+		"""Тест: используется домашний шаблон."""
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
