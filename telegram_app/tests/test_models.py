8e48b3d12cb0f28829aa1bcf0c22d84f85d8f311
Narek <narekbayanduryan16@gmail.com>
2025-02-12 22:38:53 +0300

    Wrote a test to check that telegram_bot should be unique.

--- telegram_app/tests/test_models.py
+++ telegram_app/tests/test_models.py
@@ -8,10 +8,21 @@ from telegram_app.models import TelegramBot
 class TelegramBotModelTest(TestCase):
 
 
-	def test_connect_bot_with_empty_token(self):
+	def test_cannot_connect_telegram_bot_with_empty_token(self):
 		"""Тест: пытается создать обьект с пустым полем token."""
 		new_bot = TelegramBot()
 
 		with self.assertRaises(ValidationError):
-			new_bot.full_clean()
 			new_bot.save()
+
+
+	def test_duplicate_telegram_bot_are_invalid(self):
+		"""Тест: повторы подключения Telegram ботов не допустимы."""
+		TelegramBot.objects.create(
+			token="8083179427:AAF5z0kDDygySnBfzLAkYe9RFYcfcuC9pTg"
+		)
+
+		with self.assertRaises(ValidationError):
+			TelegramBot.objects.create(
+				token="8083179427:AAF5z0kDDygySnBfzLAkYe9RFYcfcuC9pTg"
+			)
