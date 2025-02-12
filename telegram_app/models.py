ac6801239416376b6f4257f9489b4dc6a709da64
Narek <narekbayanduryan16@gmail.com>
2025-02-12 21:02:59 +0300

    Created the TelegramBot model.

--- telegram_app/models.py
+++ telegram_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class TelegramBot(models.Model):
+	token = models.CharField(max_length=100, unique=True, blank=False, null=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+	class Meta:
+		db_table = 'telegram_bot'
