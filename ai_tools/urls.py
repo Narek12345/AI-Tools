5a7521f27846ee1d669aecbe1529363221c62426
Narek <narekbayanduryan16@gmail.com>
2025-02-12 23:55:50 +0300

    Corrected the path to the telegram app.

--- ai_tools/urls.py
+++ ai_tools/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core_app.urls')),
-    path('telegram', include('telegram_app.urls')),
+    path('telegram/', include('telegram_app.urls')),
 ]
