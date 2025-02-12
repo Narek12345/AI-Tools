9d38ffda7a03e9a224b795f0bc0a637e6c63f14d
Narek <narekbayanduryan16@gmail.com>
2025-02-12 11:54:15 +0300

    Updated main template test to telegram_app template test.

--- telegram_app/tests/test_views.py
+++ telegram_app/tests/test_views.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 
 
 
-class HomePageTest(TestCase):
+class TelegramPageTest(TestCase):
 	"""Тест домашней страницы."""
 
 
-	def test_uses_home_template(self):
+	def test_uses_home_telegram_template(self):
 		"""Тест: используется домашний шаблон."""
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
+		response = self.client.get('/telegram')
+		self.assertTemplateUsed(response, 'home_telegram.html')
