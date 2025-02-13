d9f2a79baca31f4ee1f9d60ce0bcc8fae27375b8
Narek <narekbayanduryan16@gmail.com>
2025-02-13 19:46:43 +0300

    Renamed the telegram bot connection view.

--- telegram_app/views.py
+++ telegram_app/views.py
@@ -6,5 +6,5 @@ def home_telegram_page(request):
 	return render(request, 'telegram_app/home_telegram.html')
 
 
-def connect_telegram_bot(request):
-	return render(request, 'telegram_app/connect_telegram_bot.html')
+def connect_bot(request):
+	return render(request, 'telegram_app/connect_bot.html')
