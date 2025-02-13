aa87e7ebc87ed329702ed6bcfdd156d42c4e4e80
Narek <narekbayanduryan16@gmail.com>
2025-02-14 00:44:49 +0300

    Apply decorator to wait.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,25 @@
+import time
+
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+
+MAX_WAIT = 5
+
+
+def wait(fn):
+	def modified_fn(*args, **kwargs):
+		start_time = time.time()
+		while True:
+			try:
+				return fn(*args, **kwargs)
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+	return modified_fn
 
 
 
