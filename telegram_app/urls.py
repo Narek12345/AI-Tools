417e888715a3fb1b5f94a2a2a6b9d1c743da5d72
Narek <narekbayanduryan16@gmail.com>
2025-02-13 19:50:12 +0300

    Fixed the bug.

--- telegram_app/urls.py
+++ telegram_app/urls.py
@@ -7,5 +7,5 @@ app_name = "telegram_app"
 
 urlpatterns = [
 	path('', views.home_telegram_page, name="home_telegram"),
-	path('connect-bot', views.connect_telegram_bot, name="connect_bot"),
+	path('connect-bot', views.connect_bot, name="connect_bot"),
 ]
