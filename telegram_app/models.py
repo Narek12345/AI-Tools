2212cf29aaad0c3f566de8dfcd9763b57c8e0c05
Narek <narekbayanduryan16@gmail.com>
2025-02-14 14:04:36 +0300

    Added a name field to the TelegramBot model.

--- telegram_app/models.py
+++ telegram_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class TelegramBot(models.Model):
+	name = models.CharField(max_length=50, blank=False, null=False)
 	token = models.CharField(max_length=100, unique=True, blank=False, null=False)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
