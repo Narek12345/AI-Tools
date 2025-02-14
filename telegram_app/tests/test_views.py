218a345bd67708d5cf5873ac1fe4d7b7cdcfdad7
Narek <narekbayanduryan16@gmail.com>
2025-02-15 00:15:56 +0300

    Wrote a unit test to check redirection after connecting telegram bot.

--- telegram_app/tests/test_views.py
+++ telegram_app/tests/test_views.py
@@ -12,7 +12,25 @@ class TelegramPageTest(TestCase):
 		self.assertTemplateUsed(response, 'telegram_app/home_telegram.html')
 
 
+
+class ConnectTelegramBotTest(TestCase):
+
+
 	def test_connect_telegram_bot(self):
 		"""Тест: подключение нового telegram bot."""
 		response = self.client.get('/telegram/connect-bot')
 		self.assertTemplateUsed(response, 'telegram_app/connect_bot.html')
+
+
+	def test_bot_connection_page(self):
+		"""Тест: перенаправление на страницу бота после успешного подключения."""
+		response = self.client.post(
+			'/telegram/connect-bot',
+			data={
+				'name': 'Telegram bot',
+				'token': '8083179427:AAF5z0kDDygySnBfzLAkYe9RFYcfcuC9pTg',
+			}
+		)
+		bot = TelegramBot.objects.first()
+
+		self.assertRedirect(response, f'/telegram/bot/{bot.id}')
