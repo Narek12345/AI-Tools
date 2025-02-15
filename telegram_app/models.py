587e288078d5d0b5625c7cf588e140742b077848
Narek <narekbayanduryan16@gmail.com>
2025-02-15 13:22:53 +0300

    Added the is_running field to the TelegramBot model.

--- telegram_app/models.py
+++ telegram_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class TelegramBot(models.Model):
 	name = models.CharField(max_length=50, blank=False, null=False)
 	token = models.CharField(max_length=100, unique=True, blank=False, null=False)
+	is_running = models.BooleanField(default=False)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
