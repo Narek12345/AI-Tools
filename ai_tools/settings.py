f790454018f892e6bc08df9a5fbc27f85063e414
Narek <narekbayanduryan16@gmail.com>
2025-02-17 20:26:45 +0300

    Download daphne and configured it.

--- ai_tools/settings.py
+++ ai_tools/settings.py
@@ -35,6 +35,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Other apps:
+    'channels',
+    "daphne",
+    
     # Django apps:
     'django.contrib.admin',
     'django.contrib.auth',
@@ -131,3 +135,6 @@ STATIC_ROOT = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Daphne.
+ASGI_APPLICATION = "ai_tools.asgi.application"
