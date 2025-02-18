d5997a8d84d6127658bc6c27efdbb3fd27541640
Narek <narekbayanduryan16@gmail.com>
2025-02-18 16:35:12 +0300

    Removed unnecessary routing.

--- ai_tools/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from telegram_app import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/telegram_bot/status/(?P<bot_id>\d+)/$', consumers.TelegramBotStatusConsumer.as_asgi()),
-]
