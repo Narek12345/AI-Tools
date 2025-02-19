6fc9ae4103f4833226998b14eb3c675252a83638
Narek <narekbayanduryan16@gmail.com>
2025-02-19 14:55:38 +0300

    Added in 'ConnectTelegramBotForm' my labels.

--- functional_tests/test_telegram_app.py
+++ functional_tests/test_telegram_app.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-
 from .base import FunctionalTest, wait
 
 
--- telegram_app/forms.py
+++ telegram_app/forms.py
@@ -5,24 +5,30 @@ from telegram_app.models import TelegramBot
 
 
 class ConnectTelegramBotForm(forms.ModelForm):
-	"""Форма для добавления нового Telegram бота."""
+    """Форма для добавления нового Telegram бота."""
 
-	class Meta:
-		model = TelegramBot
-		fields = ('name', 'token')
-		widgets = {
-			'name': forms.fields.TextInput(
-				attrs={
-					'placeholder': 'Введите название бота',
-				}
-			),
-			'token': forms.fields.TextInput(
-				attrs={
-					'placeholder': 'Вставьте токен вашего бота',
-				}
-			)
-		}
-		error_messages = {
-			'name': {'required': 'Поле должно быть заполнено'},
-			'token': {'required': 'Поле должно быть заполнено'},
-		}
+    class Meta:
+        model = TelegramBot
+        fields = ('name', 'token')
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Введите название бота',
+                    'class': 'form-control'
+                }
+            ),
+            'token': forms.TextInput(
+                attrs={
+                    'placeholder': 'Вставьте токен вашего бота',
+                    'class': 'form-control'
+                }
+            )
+        }
+        labels = {
+            'name': 'Название бота',
+            'token': 'Токен бота'
+        }
+        error_messages = {
+            'name': {'required': 'Поле должно быть заполнено'},
+            'token': {'required': 'Поле должно быть заполнено'},
+        }
