0ab9a0f3e86828d83c5b01093c56b3969e0119f9
Narek <narekbayanduryan16@gmail.com>
2025-02-19 12:36:08 +0300

    Wrote a new view 'no_function' and implemented it where there is no functionality yet.

--- core_app/urls.py
+++ core_app/urls.py
@@ -7,4 +7,5 @@ app_name = 'core_app'
 
 urlpatterns = [
 	path('', views.home_page, name='home'),
+	path('no_function', views.no_function, name="no_function"),
 ]
--- core_app/views.py
+++ core_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def home_page(request):
 	"""Представление домашней страницы."""
 	return render(request, 'core_app/home.html')
+
+
+def no_function(request):
+	return render(request, 'core_app/no_function.html')
